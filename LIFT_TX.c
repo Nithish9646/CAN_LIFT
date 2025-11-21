3d26332b3af8918657a5f2a7ca816b55c5b2fa45
Nithish9646 <nithishp9646@gmail.com>
2025-11-21 17:34:29 +0530

    Add files via upload

---
+++ LIFT_TX.c
@@ -0,0 +1,185 @@
+#include<lpc21xx.h>
+#include "lcddriver.h"
+#define LED1 1<<18
+#define LED2 1<<19
+#define LED3 1<<20
+#define UP 10
+#define DOWN 11
+#define EMG 12
+#define BUZZER 1<<16
+#define MOTOR 1<<17
+#define u8 unsigned int
+typedef struct can_tx
+{
+u8 id;
+u8 rtr;
+u8 dlc;
+u8 data1;
+u8 data2;
+}tx;
+void can_init(void);
+void can_tx(tx);
+
+int main()
+{
+     static int count=0;
+	tx m1;	
+	IODIR0|=LED1|LED2|LED3|BUZZER|MOTOR;
+	IOSET0=LED1|LED2|LED3;
+	can_init();
+	LCD_INIT();
+	m1.id=0x1AF;;
+	m1.rtr=0;
+	m1.dlc=4;
+	LCD_CMD(0X01);
+		LCD_CMD(0X80);
+		STRING("GROUND FLOOR");
+	while(1)
+	{
+	  if(((IOPIN0>>UP)&1)==0)
+	  {
+	  delay(250);
+	  count++;
+	    if(count>3)
+			count=3;
+		  
+		if(count==1)
+		{
+			IOCLR0=BUZZER;
+		IOSET0=MOTOR;
+		delay(400);
+		IOCLR0=MOTOR;
+		LCD_CMD(0X01);
+		LCD_CMD(0X80);
+		STRING("FIRST FLOOR");
+		IOCLR0=LED1;
+		IOSET0=LED2;
+		IOSET0=LED3;
+		}
+		else if(count==2)
+		{
+		IOCLR0=BUZZER;
+		IOSET0=MOTOR;
+		delay(400);
+		IOCLR0=MOTOR;
+		LCD_CMD(0X01);
+		LCD_CMD(0X80);
+		STRING("SECOUND FLOOR");
+		IOCLR0=LED2;
+		IOSET0=LED1;
+		IOSET0=LED3;
+		}
+		else if(count==3)
+		{
+		IOCLR0=BUZZER;
+		IOSET0=MOTOR;
+		delay(400);
+		IOCLR0=MOTOR;
+		LCD_CMD(0X01);
+		LCD_CMD(0X80);
+		STRING("THIRD FLOOR");
+		IOCLR0=LED3;
+		IOSET0=LED2;
+		IOSET0=LED1;
+		}
+		m1.data1=count;
+		m1.data2=0;
+	    can_tx(m1);
+	  }
+	  else if(((IOPIN0>>DOWN)&1)==0)
+	  {
+	  delay(250);
+	  count--;
+		  if(count<0)
+			  count=0;
+	  	if(count==1)
+		{
+		IOCLR0=BUZZER;
+		IOSET0=MOTOR;
+		delay(400);
+		IOCLR0=MOTOR;
+		LCD_CMD(0X01);
+		LCD_CMD(0X80);
+		STRING("FIRST FLOOR");
+		IOCLR0=LED1;
+		IOSET0=LED2;
+		IOSET0=LED3;
+		}
+	    else if(count==2)
+		{
+		IOCLR0=BUZZER;
+		IOSET0=MOTOR;
+		delay(400);
+		IOCLR0=MOTOR;
+		LCD_CMD(0X01);
+		LCD_CMD(0X80);
+		STRING("SECOUND FLOOR");
+		IOCLR0=LED2;
+		IOSET0=LED1;
+		IOSET0=LED3;
+		}
+		else if(count==3)
+		{
+		IOCLR0=BUZZER;
+		IOSET0=MOTOR;
+		delay(400);
+		IOCLR0=MOTOR;
+		LCD_CMD(0X01);
+		LCD_CMD(0X80);
+		STRING("THIRD FLOOR");
+		IOCLR0=LED3;
+		IOSET0=LED2;
+		IOSET0=LED1;
+		}
+		else if(count==0)
+		{
+		IOCLR0=BUZZER;
+		IOSET0=MOTOR;
+		delay(400);
+		IOCLR0=MOTOR;
+		LCD_CMD(0X01);
+		LCD_CMD(0X80);
+		STRING("GROUND FLOOR");
+		}
+		m1.data1=count;
+		m1.data2=0;
+		can_tx(m1);
+	  }
+	  else if(((IOPIN0>>EMG)&1)==0)
+	  {
+		IOSET0=MOTOR;
+		delay(400);
+		IOCLR0=MOTOR;
+		count=0;
+		LCD_CMD(0X01);
+		LCD_CMD(0X80);
+		STRING("GROUND FLOOR");
+		IOSET0=BUZZER;
+	  }
+	}
+}
+void can_init(void)
+{
+	PINSEL1|=0X14000;
+	VPBDIV=1;
+	C2MOD=0X01;
+	AFMR=0X02;
+	C2BTR=0X001C001D;
+	C2MOD=0X00;
+}
+void can_tx(tx m1)
+{
+	C2TID1=m1.id;
+	C2TFI1=(m1.dlc<<16);
+	if(m1.rtr==0)
+	{
+	C2TFI1&=~(1<<30);
+	C2TDA1=m1.data1;
+	C2TDB1=m1.data2;
+	}
+	else
+	C2TFI1|=(1<<30);
+	C2CMR=(1<<0)|(1<<5);
+while((C2GSR&(1<<3))==0);
+}
+
